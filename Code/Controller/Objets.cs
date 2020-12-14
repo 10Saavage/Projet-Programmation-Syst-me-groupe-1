82c9de447ebac24b5e927303de3ac6a91df09ab2
10Saavage <manoel.toussi.cm@viacesi.fr>
2020-12-14 11:45:07 +0100

    meilleure structuration du code v2.0

--- Code/Controller/Objets.cs
+++ Code/Controller/Objets.cs
@@ -27,102 +27,11 @@ namespace Controller
         }
     }
 
-    //Classe d'objets divers, n'est exploitée que pour l'argent mais peut être implémentée pour n'importe quel autre objet
-    public class Divers: Objet
-    {
-        public string type;
-        public int quantity;
 
-        public Divers(string type, Entity conteneur, Loader loader, int quantity = 1) : base(conteneur, loader)
-        {
-            this.type = type;
-            this.quantity = quantity;
-        }
 
-        public override List<string[]> ReturnInformations()
-        {
-            List<string[]> infosToReturn = new List<string[]>();
-
-            infosToReturn.Add(new string[] { "ID", id.ToString() });
-            infosToReturn.Add(new string[] { "Type", type });
-            infosToReturn.Add(new string[] { "Quantité", quantity.ToString() });
-
-            return infosToReturn;
-        }
-    }
-
-    //Classe de la nourriture
-    public class Nourriture: Objet
-    {
-        public string nom;
-        private string stockage;
-
-        public Nourriture(string nom, string stockage, Entity conteneur, Loader loader) : base(conteneur, loader)
-        {
-            this.nom = nom;
-            this.stockage = stockage;
-        }
-
-        public override List<string[]> ReturnInformations()
-        {
-            List<string[]> infosToReturn = new List<string[]>();
-
-            infosToReturn.Add(new string[] { "ID", id.ToString() });
-            infosToReturn.Add(new string[] { "Nom", nom });
-            infosToReturn.Add(new string[] { "Stockage", stockage });
-
-            return infosToReturn;
-        }
-    }
-
-    //Classe de matériel (élément étant utilisé et pouvant être propre ou sale, comme les couverts, les assiettes, les nappes...)
-    public class Materiel: Objet
-    {
-        private bool propre;
-
-        public Materiel(Entity conteneur, Loader loader, bool propre = true) : base(conteneur, loader)
-        {
-            this.propre = propre;
-        }
-
-        public override List<string[]> ReturnInformations()
-        {
-            List<string[]> infosToReturn = new List<string[]>();
-
-            infosToReturn.Add(new string[] { "ID", id.ToString() });
-            infosToReturn.Add(new string[] { "Propre", propre.ToString() });
-
-            return infosToReturn;
-        }
-
-        public void Salir()
-        {
-            propre = false;
-        }
-
-        public void Nettoyer()
-        {
-            propre = true;
-        }
-
-        public bool GetProprete()
-        {
-            return propre;
-        }
-    }
-
-    //Classe d'ustensiles (couverts, casseroles...)
-    public class Ustensile: Materiel
-    {
-        public string ustensileType;
-        public Ustensile(Entity conteneur, Loader loader, string ustensileType, bool propre = true) : base(conteneur, loader, propre)
-        {
-            this.ustensileType = ustensileType;
-        }
-    }
 
     //Classe d'objet conteneur, objet qui va contenir d'autres objets (assiette, qui va contenir des aliments...)
-    public class ObjetConteneur: Materiel
+    public class ObjetConteneur : Materiel
     {
         //If it can contain solids or liquids
         public string contenuType;
@@ -151,63 +60,4 @@ namespace Controller
             return contenuNourriture;
         }
     }
-
-    //Classe de Plat, étant une assiette avec des aliments transformée (pour simplifier le code)
-    public class Plat: Objet
-    {
-        private ObjetConteneur platConteneur;
-        public int platId;
-        public string nomPlat;
-
-        public Plat(string nomPlat, int platId, ObjetConteneur platConteneur, Entity conteneur, Loader loader) : base(conteneur, loader)
-        {
-            this.nomPlat = nomPlat;
-            this.platId = platId;
-            this.platConteneur = platConteneur;
-        }
-
-        public override List<string[]> ReturnInformations()
-        {
-            List<string[]> infosToReturn = new List<string[]>();
-
-            infosToReturn.Add(new string[] { "ID", id.ToString() });
-            infosToReturn.Add(new string[] { "Plat", nomPlat });
-
-            return infosToReturn;
-        }
-    }
-
-    //Classe de la carte, qui contiens tous les menus
-    public class Carte: Objet
-    {
-        private List<List<String>> menus;
-
-        public Carte(Entity conteneur, Loader loader, List<List<String>> menus) : base(conteneur, loader)
-        {
-            this.menus = menus;
-        }
-
-        public override List<string[]> ReturnInformations()
-        {
-            List<string[]> infosToReturn = new List<string[]>();
-
-            infosToReturn.Add(new string[] { "ID", id.ToString() });
-
-            int i = 0;
-
-            foreach(List<String> menu in menus)
-            {
-                i++;
-                infosToReturn.Add(new string[] { "Menu "+i, menu[0]+" "+menu[1]+" "+menu[2] });
-            }
-
-            return infosToReturn;
-        }
-
-        public List<List<String>> GetMenus()
-        {
-            return menus;
-        }
-    }
-
 }
--- Code/Controller/Serveur.cs
+++ Code/Controller/Serveur.cs
@@ -6,7 +6,149 @@ using System.Threading.Tasks;
 
 namespace Controller
 {
-    class Serveur
+    //Classe serveur, qui apportera les plats aux clients
+    public class Serveur : PersonnelSalle
     {
+        private string etat;
+        private Comptoir comptoir;
+        private Table targetTable;
+        private bool hasPlats;
+
+        public Serveur(string nom, string prenom, int age, int[] position, Loader loader) : base(nom, prenom, age, position, loader)
+        {
+            etat = "idle";
+            comptoir = loader.GetWorld().GetComptoir();
+        }
+
+        //Fonction appelée à chaque tic, définis l'état du serveur et permet de vérifier périodiquement l'état des clients pour prendre des décisions en conséquence
+        public override void UpdatePersonnage(int delta)
+        {
+            switch (etat)
+            {
+                case "idle":
+
+                    CheckPlatsClients();
+
+                    break;
+
+                case "apportePain":
+                    break;
+
+                case "apportePlat":
+
+                    if (hasPlats == false)
+                    {
+                        if (GetPosition() != comptoir.GetPointAccesRestaurant())
+                        {
+                            MoveTo(comptoir.GetPointAccesRestaurant());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Serveur " + id + " a prit plats pour table " + targetTable.GetNumTable());
+                            foreach (Client client in targetTable.GetClients())
+                            {
+                                inventaire.Add(comptoir.stockPlats.TakeContent(comptoir.stockPlats.FindContent(new Predicate<object>(o => ((Plat)o).nomPlat == client.GetPlatAttente()))));
+                            }
+                            hasPlats = true;
+                        }
+                    }
+                    else
+                    {
+                        if (GetPosition() != targetTable.GetPosition())
+                        {
+                            MoveTo(targetTable.GetPosition());
+                        }
+                        else
+                        {
+                            foreach (Client client in targetTable.GetClients())
+                            {
+                                Plat plat = (Plat)inventaire.Find(o => ((Plat)o).nomPlat == client.GetPlatAttente());
+                                inventaire.Remove(plat);
+                                client.RecevoirPlat(plat);
+                            }
+                            targetTable.serveurReservee = false;
+                            targetTable = null;
+                            hasPlats = false;
+                            etat = "idle";
+                        }
+                    }
+                    break;
+
+                case "debarasseRepas":
+                    break;
+            }
+        }
+
+        //Fonction de retour d'informations pour la vue
+        public override List<string[]> ReturnInformations()
+        {
+            List<string[]> infosToReturn = new List<string[]>();
+
+            infosToReturn.Add(new string[] { "ID", id.ToString() });
+            infosToReturn.Add(new string[] { "Etat", etat });
+            //infosToReturn.Add(new string[] { "Table ciblée", targetTable.GetNumTable().ToString() });
+            infosToReturn.Add(new string[] { "A des plats", hasPlats.ToString() });
+
+            return infosToReturn;
+        }
+
+        //Check de l'état des clients pour chaque table. Permet de savoir quand des clients sont en train d'attendre un plat
+        private List<Table> CheckTablesClientsEtat(string etatToCheck)
+        {
+            List<Table> tablesRemplies = loader.GetWorld().getRestaurant().GetFilledTables();
+            List<Table> tablesCorrespondent = new List<Table>();
+
+            foreach (Table table in tablesRemplies)
+            {
+                bool clientsCorrespondent = true;
+
+                foreach (Client client in table.GetClients())
+                {
+                    if (client.GetEtat() != etatToCheck)
+                    {
+                        clientsCorrespondent = false;
+                        break;
+                    }
+                }
+                if (clientsCorrespondent == true)
+                {
+                    tablesCorrespondent.Add(table);
+                }
+            }
+            return tablesCorrespondent;
+        }
+
+        //Check les plats que les clients attendent, afin de les apporter si ils la cuisine les a préparés
+        private void CheckPlatsClients()
+        {
+            List<Table> tablesCorrespondent = CheckTablesClientsEtat("attendsRepas");
+
+            if (tablesCorrespondent.Count() > 0)
+            {
+                foreach (Table table in tablesCorrespondent)
+                {
+                    bool platsPrets = true;
+                    foreach (Client client in table.GetClients())
+                    {
+                        if (comptoir.HasPlat(client.GetPlatAttente()) == false)
+                        {
+                            platsPrets = false;
+                        }
+                    }
+
+                    if (platsPrets)
+                    {
+                        if (table.serveurReservee == false)
+                        {
+                            table.serveurReservee = true;
+                            targetTable = table;
+                            etat = "apportePlat";
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
     }
 }
